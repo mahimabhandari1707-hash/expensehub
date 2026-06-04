e1b868d8a1f582e670513cd8a3a0879ad99436f7
Mahima Bhandari <mahimabhandari1707@gmail.com>
2026-06-04 22:20:15 +0530

    Initial ExpenseHub project

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const signupBtn = document.querySelector(".signup-btn");
 signupBtn.addEventListener("click", () => {
     alert("Sign Up Feature Coming Soon!");
 });
+
+const heroBtn = document.querySelector(".hero-btn");
+heroBtn.addEventListener("click", () => {
+    console.log("Hero Button Clicked");
+});
