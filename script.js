5f2af17d8f56e75328109a2e2593a9793d5286bd
Mahima Bhandari <mahimabhandari1707@gmail.com>
2026-06-05 14:09:19 +0530

    Added hero section

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const heroBtn = document.querySelector(".hero-btn");
 heroBtn.addEventListener("click", () => {
     console.log("Hero Button Clicked");
 });
+
+const dashboard = 
+document.querySelector(".dashboard-img");
+
+dashboard.addEventListener("mouseover", () => {
+    dashboard.style.transform =
+    "scale(1.03)";
+});
+
+dashboard.addEventListener("mouseout", () => {
+    dashboard.style.transform = 
+    "scale(1)";
+});
